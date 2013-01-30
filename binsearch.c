1eeb042200014093764b1dd0bcb645a69509fee7
Steve Howell <steve@strings.com>
2013-01-29 20:03:12 -0800

    wip

--- binsearch.c
+++ binsearch.c
@@ -10,6 +10,18 @@ int search(int *arr, int low, int high, int val) {
     return search(arr, mid + 1, high, val);
 }
 
+int search2(int *arr, int low, int high, int val) {
+    // return smallest i such that a[i] >= val
+    //   and low <= i < high
+    // if all a[i] < val, return high
+    if (low == high) return high;
+    if (arr[low] >= val) return low;
+    if (low == high - 1) return high;
+    int mid = (low + high) / 2;
+    if (arr[mid] <= val) return search2(arr, mid, high, val);
+    return search2(arr, low, mid, val);
+}
+
 int main(int argc, char **argv) {
     int arr[] = {1, 3, 4, 7, 11, 18};
     int len = 6;
@@ -18,11 +30,11 @@ int main(int argc, char **argv) {
 
     for (i = 0; i < len; ++i) {
         val = arr[i];
-        printf("%d %d\n", val, search(arr, 0, len, val));
+        printf("%d %d\n", search2(arr, 0, len, val), val);
     }
-    val = 0; printf("%d %d\n", val, search(arr, 0, len, val));
-    val = 10; printf("%d %d\n", val, search(arr, 0, len, val));
-    val = 20; printf("%d %d\n", val, search(arr, 0, len, val));
+    val = 0; printf("%d %d\n", search2(arr, 0, len, val), val);
+    val = 10; printf("%d %d\n", search2(arr, 0, len, val), val);
+    val = 20; printf("%d %d\n", search2(arr, 0, len, val), val);
 
     return 0;
 }
